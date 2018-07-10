a3fedabfd432ad843771a878681d941f94fa1c5f
R <merinogeospatial@gmail.com>
2018-07-10 14:52:57 -0400

    start word logic

--- word.js
+++ word.js
@@ -1,16 +1,11 @@
-// Requiring our CastMember constructor function we exported from castMember.js
 var Letter = require("./letter");
 
-// Constructor function for creating Movie objects
 var Word = function() {
-  // this.cast will hold all of our CastMember objects
+
+    this.letterArr = [];
 
 
-  // This method that creates a CastMember object from the constructor function we required and pushes it to the `this.cast` array
-  this.addCastMember = function(gender, name, role) {
-    this.cast.push(new CastMember(gender, name, role));
-  };
+
 };
 
-// Exporting the Movie constructor which we will use in main.js
-module.exports = Movie;
+module.exports = Word;
